6a0e48a8a146f110864f64b92ee7eef45fbb7d7d
dengly <denglylove@gmail.com>
2019-01-28 16:28:51 +0800

    去掉虹软相关key

--- app/src/main/java/com/example/l/mobilefacenet/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.l.mobilefacenet.util;
-
-public class Constants {
-    public static final String APP_ID = "8Z46HLRKuswAFbMgWRAkjwYKhq5efDj73677jKShT4r2";
-    public static final String SDK_KEY = "EFUox52DkpDikCDmQPLikgqkfxhXHoL8pyAX5ZBci2H";
-}
